4f1e66e4396567782ea4f8cf79d3e1976944bfcf
rahulnath401 <59950336+rahulnath401@users.noreply.github.com>
2022-09-09 10:00:37 +0530

    Add files via upload

---
+++ inc/page_id_helper_functions.h
@@ -0,0 +1,10 @@
+#ifndef PAGE_ID_HELPER_FUNCTIONS_H
+#define PAGE_ID_HELPER_FUNCTIONS_H
+
+#include<buffer_pool_man_types.h>
+
+unsigned int hash_page_id(PAGE_ID page_id);
+
+int compare_page_id(PAGE_ID page_id1, PAGE_ID page_id2);
+
+#endif
